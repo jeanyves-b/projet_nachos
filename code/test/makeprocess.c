bb74da872b972e6f9d503dfd24e1d8ab8c7714b3
avrilse <spiavril@sfr.fr>
2015-01-13 10:07:31 +0100

    rajout des tests

---
+++ code/test/makeprocess.c
@@ -0,0 +1,19 @@
+#include "syscall.h"
+
+ void thread2(void *n){
+	 int i=0;
+	 while (i<10) {
+		PutChar('0'+i);
+		i++;
+	}
+}
+
+
+int main()
+{
+  UserThreadCreate(thread2, 0);
+
+  ForkExec("userpages0");
+  ForkExec("userpages1");
+  return 0;
+}
