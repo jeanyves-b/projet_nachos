636d1e8a6fcfa82086538f3fcea437f67d0462bd
jeanyves-b <guanach@gmail.com>
2015-01-19 14:43:02 +0100

    patate

--- code/test/etape2/tester.cc
+++ code/test/etape2/tester.cc
@@ -34,14 +34,15 @@ int main()
 	int nbfautes = 0;
 	char filein[10] = "in0.txt\0";
 	char fileout[10] = "out0.txt\0";
+	char fileexpe[10] = "expe0.txt\0";
 	char *argv[];
 	char *envp[] = {NULL};
 
 	for (int i=0; i<NBTEST; i++)
 	{
-
 		filein[3] = *itoa(i);
 		fileout[4] = *itoa(i);
+		fileexpe[5]
 		ifstream in(filein);
 		ifstream out(fileout);
 
@@ -50,7 +51,7 @@ int main()
 
 		while(!(in.eof || out.eof))
 		{
-			getline(in, entree);
+			getline(expe, entree);
 			getline(out, sortie);
 
 			printf("test N° : %4d ", testnb++);
@@ -61,5 +62,4 @@ int main()
 
 	//affichage des résultats de tests
 	printf("%d tests done\n%d faults", testnb, nbfautes);
-	
 }
