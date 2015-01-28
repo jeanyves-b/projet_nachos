09e4566ce145123b05dc746d526dfb0cc5537072
jeanyves-b <guanach@gmail.com>
2015-01-28 15:54:02 +0100

    dernière partie du rapport

--- code/test/etape3/lancer.sh
+++ code/test/etape3/lancer.sh
@@ -2,14 +2,24 @@
 
 echo "lancement des programme de test de l'étape 3"
 echo "lancement de userpage0"
-./../../build/nachos-step6 -x ./../../build/userpage0 -rs 23
+./../../build/nachos-step6 -cp ./../../build/etape3/userpage0 a
+./../../build/nachos-step6 -x a -rs 23
+./../../build/nachos-step6 -r a
 echo "lancement de makethread"
-./../../build/nachos-step6 -x ./../../build/makethread -rs 23
+./../../build/nachos-step6 -cp ./../../build/etape3/makethread a
+./../../build/nachos-step6 -x a -rs 23
+./../../build/nachos-step6 -r a
 echo "lancement de threadautoexit"
-./../../build/nachos-step6 -x ./../../build/threadautoexit -rs 23
+./../../build/nachos-step6 -cp ./../../build/etape3/threadautoexit a
+./../../build/nachos-step6 -x a -rs 23
+./../../build/nachos-step6 -r a
 echo "lancement de threadnojoin"
-./../../build/nachos-step6 -x ./../../build/threadnojoin -rs 23
+./../../build/nachos-step6 -cp ./../../build/etape3/threadnojoin a
+./../../build/nachos-step6 -x a -rs 23
+./../../build/nachos-step6 -r a
 echo "lancement de threaderror"
-./../../build/nachos-step6 -x ./../../build/threaderror -rs 23
+./../../build/nachos-step6 -cp ./../../build/etape3/threaderror a
+./../../build/nachos-step6 -x a -rs 23
+./../../build/nachos-step6 -r a
 echo "fin du lancer de l'étape 3"
 echo ""
