341b18d38dcc036e4e2eaa7e0972aa68c40069b4
jeanyves-b <guanach@gmail.com>
2015-01-28 13:18:30 +0100

    définition des lanceur de tests

---
+++ code/test/etape5/lancer.sh
@@ -0,0 +1,15 @@
+#lancer de l'étape 5
+
+echo "lancement des programme de test de l'étape 5"
+echo "lancement de mkdir"
+./../../build/nachos-step6 -x ./../../build/mkdir
+echo "lancement de rmdir"
+./../../build/nachos-step6 -x ./../../build/rmdir
+echo "lancement de mkfile"
+./../../build/nachos-step6 -x ./../../build/mkfile
+echo "lancement de rmfile"
+./../../build/nachos-step6 -x ./../../build/rmfile
+echo "lancement de cd"
+./../../build/nachos-step6 -x ./../../build/cd
+echo "fin du lancer de l'étape 5"
+echo ""
