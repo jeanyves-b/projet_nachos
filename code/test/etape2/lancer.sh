722cac1c752b6692d2d476904fba0cc68b758da9
jeanyves-b <guanach@gmail.com>
2015-01-29 12:58:08 +0100

    fin test étape2

--- code/test/etape2/lancer.sh
+++ code/test/etape2/lancer.sh
@@ -28,17 +28,9 @@ echo "lancement de putstring"
 ./../../build/nachos-final -x a -rs 23
 ./../../build/nachos-final -r a
 echo "lancement de la console synchrone avec les fichiers in0, in1, in2, in3"
-./../../build/nachos-final -cp in0 in0
-./../../build/nachos-final -sc in0 out0
-./../../build/nachos-final -r in0
-./../../build/nachos-final -cp in1 in1
-./../../build/nachos-final -sc in1 out1
-./../../build/nachos-final -r in1
-./../../build/nachos-final -cp in2 in2
-./../../build/nachos-final -sc in2 out2
-./../../build/nachos-final -r in2
-./../../build/nachos-final -cp in3 in3
-./../../build/nachos-final -sc in3 out3
-./../../build/nachos-final -r in3
+./../../build/nachos-final -sc < in0.txt > out0.txt
+./../../build/nachos-final -sc < in1.txt > out1.txt
+./../../build/nachos-final -sc < in2.txt > out2.txt
+./../../build/nachos-final -sc < in3.txt > out3.txt
 echo "fin du lancer de l'étape 2"
 echo ""
